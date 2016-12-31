adf8cbf4a2f2b36616415fa0cc2757f9cb72ae06
Victor Vicente Palacios <victorvicpal@gmail.com>
2016-12-31 11:49:02 +0100

    find path between users (recursive)

---
+++ find_path.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 31 10:14:20 2016
+
+@author: victor
+"""
+
+# ----------------------------------------------------------------------------- 
+# find_path_to_friend(network, user_A, user_B): 
+#   Finds a connections path from user_A to user_B. It has to be an existing 
+#   path but it DOES NOT have to be the shortest path.
+#   
+# Arguments:
+#   network: The network you created with create_data_structure. 
+#   user_A:  String holding the starting username ("Abe")
+#   user_B:  String holding the ending username ("Zed")
+# 
+# Return:
+#   A list showing the path from user_A to user_B.
+#   - If such a path does not exist, return None.
+#   - If user_A or user_B is not in network, return None.
+#
+# Sample output:
+#   >>> print find_path_to_friend(network, "Abe", "Zed")
+#   >>> ['Abe', 'Gel', 'Sam', 'Zed']
+#   This implies that Abe is connected with Gel, who is connected with Sam, 
+#   who is connected with Zed.
+# 
+# NOTE:
+#   You must solve this problem using recursion!
+# 
+# Hints: 
+# - Be careful how you handle connection loops, for example, A is connected to B. 
+#   B is connected to C. C is connected to B. Make sure your code terminates in 
+#   that case.
+# - If you are comfortable with default parameters, you might consider using one 
+#   in this procedure to keep track of nodes already visited in your search. You 
+#   may safely add default parameters since all calls used in the grading script 
+#   will only include the arguments network, user_A, and user_B.
+
+def find_path_to_friend(network, user_A, user_B,path=[]):
+        path= path + [user_A]
+        if user_A==user_B:
+            return path
+        if user_A not in network:
+            return None
+        for node in network[user_A]['friends']:
+            if node not in path:
+                newpath = find_path_to_friend(network, node, user_B,path)
+                if newpath: return newpath
+        return None
