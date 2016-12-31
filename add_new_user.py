9ff45dc804101d6b96e6ee02593ff20d35a0e48a
Victor Vicente Palacios <victorvicpal@gmail.com>
2016-12-31 17:45:22 +0100

    Update add_new_user

--- add_new_user.py
+++ add_new_user.py
@@ -6,14 +6,26 @@ Created on Sat Dec 31 12:07:25 2016
 @author: victor
 """
 
-# Make-Your-Own-Procedure (MYOP)
 # ----------------------------------------------------------------------------- 
-# Your MYOP should either perform some manipulation of your network data 
-# structure (like add_new_user) or it should perform some valuable analysis of 
-# your network (like path_to_friend). Don't forget to comment your MYOP. You 
-# may give this procedure any name you want.
+# add_new_user(network, user, games): 
+#   Creates a new user profile and adds that user to the network, along with
+#   any game preferences specified in games. Assume that the user has no 
+#   connections to begin with.
+# 
+# Arguments:
+#   network: the gamer network data structure
+#   user:    a string containing the name of the user to be added to the network
+#   games:   a list of strings containing the user's favorite games, e.g.:
+#		     ['Ninja Hamsters', 'Super Mushroom Man', 'Dinosaur Diner']
+#
+# Return: 
+#   The updated network with the new user and game preferences added. The new user 
+#   should have no connections.
+#   - If the user already exists in network, return network *UNCHANGED* (do not change
+#     the user's game preferences)
 
-def add_new_user(network,new_user,games=[]):
+
+def add_new_user(network,new_user,games):
     dicn={'friends':[],'games':games}
     if new_user in network:
         print('The User "',new_user,'" exists already')
@@ -22,5 +34,4 @@ def add_new_user(network,new_user,games=[]):
 
 add_new_user(net,'Viti',['Ninja Hamsters','Seven Schemers'])
 
-
-        
+print(net['Viti'])
