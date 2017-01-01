ce475bc8dd88dd004db7f131f1825735342e9898
Victor Vicente Palacios <victorvicpal@gmail.com>
2017-01-01 22:26:34 +0100

    manip update

--- data_analysis_course/manip_data.py
+++ data_analysis_course/manip_data.py
@@ -15,22 +15,38 @@ def unique_members(table,key):
     for i in range(0,len(table)):
         if table[i][key] not in list_members:
             list_members.append(table[i][key])
-    return len(list_members)
+    return list_members,len(list_members)
 
 enrollment_num_rows = len(enrollments)             # Replace this with your code
-enrollment_num_unique_students = unique_members(enrollments,'account_key')  # Replace this with your code
+enrollment_num_unique_students = unique_members(enrollments,'account_key')[1]
+unique_enrollment_members=  unique_members(enrollments,'account_key')[0]# Replace this with your code
 
 engagement_num_rows = len(daily_engagement)             # Replace this with your code
-engagement_num_unique_students = unique_members(daily_engagement,'acct')  # Replace this with your code
+engagement_num_unique_students = unique_members(daily_engagement,'acct')[1]  # Replace this with your code
+unique_engagement_members=unique_members(daily_engagement,'acct')[0]
 
 submission_num_rows = len(project_submissions)            
-submission_num_unique_students = unique_members(project_submissions,'account_key')  # Replace this with your code
-
+submission_num_unique_students = unique_members(project_submissions,'account_key')[1]  # Replace this with your code
+unique_submission_members = unique_members(project_submissions,'account_key')[0]
 
 ###############################################################################
 #################################  Solution   #################################
 
-unique_enrolled_students=set()
-for enrollment in enrollments:
-    unique_enrolled_students.add(enrollment['account_key'])
-    len(unique_enrolled_students)
+#unique_enrolled_students=set()
+#for enrollment in enrollments:
+#    unique_enrolled_students.add(enrollment['account_key'])
+#    len(unique_enrolled_students)
+
+#Change key name from daily_engagement
+
+print(daily_engagement[0].keys())
+def change_key(dictionary,new_key,old_key):
+    for i in range(0,len(dictionary)):
+        dictionary[i][new_key] = dictionary[i][old_key]
+        del dictionary[i][old_key]
+    return dictionary
+
+change_key(daily_engagement,'account_key','acct')
+print(daily_engagement[0].keys())
+
+print(daily_engagement[0]['account_key'])
